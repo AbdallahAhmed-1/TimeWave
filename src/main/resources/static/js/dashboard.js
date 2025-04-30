582b1fe4607db00f655eb277c8ed249c357731b8
Mohamed Alowais <m.alowais24@gmail.com>
2025-04-30 22:51:33 +0400

    Fixed dashboard issues

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-        window.location.href = '/login';
-        return;
-    }
-
-    fetch('/api/v1/auth/authenticate', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Unauthorized');
-            }
-            return response.json();
-        })
-        .then(user => {
-            console.log("Loaded user:", user);
-            document.getElementById('welcome').textContent = `Welcome, ${user.username}!`;
-        })
-        .catch(error => {
-            console.error(error);
-            window.location.href = '/login';
-        });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     const token = localStorage.getItem('token');
+//
+//     if (!token) {
+//         window.location.href = '/login';
+//         return;
+//     }
+//
+//     fetch('/api/v1/auth/authenticate', {
+//         method: 'GET',
+//         headers: {
+//             'Authorization': 'Bearer ' + token
+//         }
+//     })
+//         .then(response => {
+//             if (!response.ok) {
+//                 // throw new Error('Unauthorized');
+//             }
+//             return response.json();
+//         })
+//         .then(user => {
+//             console.log("Loaded user:", user);
+//             document.getElementById('welcome').textContent = `Welcome, ${user.username}!`;
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             window.location.href = '/login';
+//         });
+// });
